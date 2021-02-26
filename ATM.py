2902b5d5376b09574c08fb810a7440c0ab648bfa
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-02-26 12:42:23 -0500

    Update ATM.py
    02-26-21 Added a 'current account balance' under the user_section for user to access current balance

--- ATM.py
+++ ATM.py
@@ -5,7 +5,7 @@ class account:
         self.balance = balance
 
 
-user1 = account('user1', '1111', 5)
+user1 = account('user1', '1111', 100)
 user_username = input('Enter username: ')
 user_password = input('Enter password: ')
 
@@ -28,12 +28,17 @@ def end():
     quit()
 
 
+# New menu function to check current balance
+def account_balance():
+    print('Your current account balance is $' + str(user1.balance))
+
+
 def password_account_check():
     if user1.user == user_username and user1.password == user_password:
         print('Your current account balance is $' + str(user1.balance))
         user_selection()
     elif user1.user != user_username or user1.password != user_password:
-        print('Invalid Username and/or Password. Try again.')
+        print('Invalid Username and/or Password. Try again later.')
 
 
 # function to restart sub-menu without ending program
@@ -49,10 +54,11 @@ def user_continue():
 # user selection redirects user to selected atm function
 def user_selection():
     user_selection = input('\nWhat would you like to do?\n'
-                           'Enter 1 - To Withdraw\n'
-                           'Enter 2 - To Deposit\n'
-                           'Enter 3 - To Exit\n'
-                           '\nPlease select 1, 2 or 3: ')
+                           'Enter 1 - Withdraw\n'
+                           'Enter 2 - Deposit\n'
+                           'Enter 3 - View Account Balance\n'
+                           'Enter 4 - Exit\n'
+                           '\nPlease select 1, 2, 3 or 4: ')
     while user_selection != 0:
         if user_selection == '1':
             withdraw()
@@ -63,7 +69,12 @@ def user_selection():
             user_continue()
 
         if user_selection == '3':
+            account_balance()
+            user_continue()
+
+        if user_selection == '4':
             end()
+
         else:
             print('Invalid Choice')
             user_continue()
